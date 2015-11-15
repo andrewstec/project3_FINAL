f1bad58f74e636d3722107c057378a1d67759bb3
marrionluaka <davedsmithjr@gmail.com>
2015-11-14 17:19:22 -0800

    adding a few lines of code to js file

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,22 @@
 (function(){
+
+	// Auto focus search bar
 	document.forms[0].elements[0].focus();
+
+	// Sticky nav
+	var  $window = $(window),
+		 $nav = $("nav"),
+		 $baseTop = $nav.offset().top;
+
+	$window.scroll(function () {
+
+	    var $top = $window.scrollTop();
+
+	    if ($top >= $baseTop) {
+	        $nav.addClass("sticky");
+	    } else if ($top < $baseTop) {
+	        $nav.removeClass('sticky');
+	    }
+	});
+
 })();
